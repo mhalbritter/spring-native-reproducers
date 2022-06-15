094d2cb9443e2505b978ac3abc6848dcabdc3d52
Moritz Halbritter <mkammerer@vmware.com>
2022-06-15 09:24:30 +0200

    Add reproducer for gh-1599

---
+++ gh-1599/README.md
@@ -0,0 +1,1 @@
+See https://github.com/spring-projects-experimental/spring-native/issues/1599
---
+++ gh-1599/src/main/java/com/example/gh1599/Gh1599Application.java
@@ -0,0 +1,45 @@
+package com.example.gh1599;
+
+import java.sql.Connection;
+
+import javax.sql.DataSource;
+
+import com.zaxxer.hikari.HikariDataSource;
+
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceProperties;
+import org.springframework.context.annotation.Bean;
+import org.springframework.nativex.hint.MethodHint;
+import org.springframework.nativex.hint.NativeHint;
+import org.springframework.nativex.hint.TypeHint;
+
+@SpringBootApplication
+@NativeHint(types = @TypeHint(
+		types = HikariDataSource.class, methods = { @MethodHint(name = "<init>") })
+)
+public class Gh1599Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Gh1599Application.class, args);
+	}
+
+	@Bean
+	public DataSource datasource(DataSourceProperties dataSourceProperties) {
+		HikariDataSource dataSource = dataSourceProperties.initializeDataSourceBuilder().type(HikariDataSource.class).build();
+		dataSource.setMaximumPoolSize(Runtime.getRuntime().availableProcessors() * 2);
+
+		return dataSource;
+	}
+
+	@Bean
+	CommandLineRunner commandLineRunner(DataSource dataSource) {
+		return args -> {
+			try (Connection connection = dataSource.getConnection()) {
+				System.out.println("Got connection: " + connection);
+			}
+		};
+	}
+
+}
